4003e9055a239b01ad851c8e77cd201bf1f99474
Contoso <91586643+Acontoso@users.noreply.github.com>
2025-08-22 15:25:24 +1000

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,3 +5,7 @@ data "aws_ecs_cluster" "ecs_threat_intel" {
 data "aws_kms_key" "cmk_ssm_alias" {
   key_id = "alias/${var.ssm_cmk_kms_key_alias}"
 }
+
+data "aws_cognito_identity_pool" "identity_pool_oidc" {
+  identity_pool_name = "azure-ad-oidc-sentinel"
+}
