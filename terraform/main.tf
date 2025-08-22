b795018f18a5a36aeef3801d771beba6c8008d7e
Contoso <91586643+Acontoso@users.noreply.github.com>
2025-08-22 15:25:41 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
     bucket = "security-terraform-state-weshealth"
     key    = "statefiles/ti-runner"
